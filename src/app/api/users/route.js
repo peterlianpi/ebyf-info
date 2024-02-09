4268cf5a9ce9cdecd767bc88cd70285f14b943ad
Peter Pau Sian Lian <peterpausianlian2020@gmail.com>
2024-02-09 22:19:23 +0630

    update users fetch api for every 5 s update

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -2,10 +2,14 @@ import { UserInfo } from "@/app/models/UserInfo";
 import { mongoose } from "mongoose";
 
 export async function GET() {
-  mongoose.connect(process.env.MONGO_URL);
-
   try {
+    // Conect to the MongoDB database
+    await mongoose.connect(process.env.MONGO_URL);
+
+    // Fetch the user information from the database
     const userInfo = await UserInfo.find().lean();
+
+    // Close the user information from the database
 
     return Response.json(userInfo);
   } catch (error) {
