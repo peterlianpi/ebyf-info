7d06d20dd1a0883838dde6959b467123e416f119
Peter Pau Sian Lian <peterpausianlian2020@gmail.com>
2024-02-09 22:08:53 +0630

    update users fetch every 5 s

--- src/components/UsersContext.js
+++ src/components/UsersContext.js
@@ -8,9 +8,25 @@ export const UsersProvider = ({ children }) => {
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
-    fetch("/api/users").then((response) => {
-      response.json().then((users) => setUsers(users));
-    });
+    const fetchUsers = async () => {
+      try {
+        const response = await fetch("/api/users");
+        if (!response.ok) {
+          throw new Error("Failed to fetch users");
+        }
+        const data = await response.json();
+        setUsers(data);
+      } catch (error) {
+        console.error("Error fetching users : ", error);
+      }
+    };
+    // Fetch users initially
+    fetchUsers();
+
+    // Set up polling to fetch users every 5 seconds (adjust as needed)
+    const intervalId = setInterval(fetchUsers, 5000);
+    // Clean up interval on component unmount
+    return () => clearInterval(intervalId);
   }, []);
 
   return (
